f5875ecf51de710361bfa2923790bb8607f7df65
Tc <9336578@qq.com>
2018-08-17 19:27:23 +0800

    upd urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,1 +1,1 @@
-from django.urls import path,includefrom .user import urls as user_urlsurlpatterns = [    path('user_api/', include(user_urls)),    #path('course/', include(course_urls)),]
+from django.urls import path,includefrom .user import urls as user_urlsurlpatterns = [    path('user/', include(user_urls)),    #path('course/', include(course_urls)),]
--- education/urls.py
+++ education/urls.py
@@ -17,5 +17,5 @@ from django.urls import path,include
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('', include('apps.urls')),
+    path('api/', include('apps.urls')),
 ]
